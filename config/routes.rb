76da690792c98e511ad05d691c2a34d1306b822c
Cory Hack <cory.hack@gmail.com>
2018-11-25 21:58:10 -0500

    Customize portfolios show route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
-  resources :portfolios
+  ## Override resources values with except command
+  resources :portfolios, except: [:show]
+  ## Assign custom route to original route as new path name 
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
+  
   resources :blogs
+
   root to: 'pages#home'
+
 end
