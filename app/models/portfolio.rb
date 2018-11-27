2870741a89a59ab7a652fb86a1c99c8deed01cc1
Cory Hack <cory.hack@gmail.com>
2018-11-27 13:35:43 -0500

    Add Placeholder concern for image sizing

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,14 +1,13 @@
 class Portfolio < ApplicationRecord
-
+  include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
 
   after_initialize :set_defaults
 
   def set_defaults
-    self.main_image ||=  "https://via.placeholder.com/600x400.png
-C/O https://placeholder.com/"
-    self.thumb_image ||= "https://via.placeholder.com/350x200.png
-C/O https://placeholder.com/"
+    self.main_image ||= Placeholder.image_generator(height: '600', width: '400') 
+    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
+
 ## ||= operator is like saying
 # if self.main_image == nil
 #   self.main_image = "url"
