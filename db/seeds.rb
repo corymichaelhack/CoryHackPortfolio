7de0019fa858ea4cb6bc20514e1ff158cc0a7e62
Cory Hack <cory.hack@gmail.com>
2018-11-27 17:29:54 -0500

    Add technologies model, with portfolio, add technology seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -57,3 +57,11 @@ C/O https://placeholder.com/"
 end
 
 puts "1 Javascript portfolio items created"
+
+3.times do |technology|
+    Portfolio.last.technologies.create!(
+        name: "Technology #{technology}"
+        )
+end
+
+puts "3 technologies created"
