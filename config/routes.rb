42a63bb275b8a831e40b2f8343260f0ffa2343be
Cory Hack <cory.hack@gmail.com>
2018-11-28 10:05:43 -0500

    Customize registration, login and logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   ## Override resources values with except command
   resources :portfolios, except: [:show]
   ## Assign custom route to original route as new path name 
