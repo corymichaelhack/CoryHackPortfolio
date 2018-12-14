214c471f996939ffdb12f470e22c88b3a3b47eef
Cory Hack <cory.hack@gmail.com>
2018-12-13 22:48:22 -0500

    Update blogs sidebar to include topics that are populated

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,4 +1,5 @@
 class TopicsController < ApplicationController
+  before_action :set_sidebar_topics
   layout "blog"
   def index
     @topics = Topic.all
@@ -7,11 +8,17 @@ class TopicsController < ApplicationController
   def show
     @topic = Topic.find(params[:id])
 
-    
+
     if logged_in?(:site_admin)
       @blogs = @topic.blogs.recent.page(params[:page]).per(3)
     else
        @blogs = @topic.blogs.published.recent.page(params[:page]).per(3)
     end 
+  end
+
+  private
+
+  def set_sidebar_topics
+    @sidebar_topics = Topic.with_blogs
   end
 end
