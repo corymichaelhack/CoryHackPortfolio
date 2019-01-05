558d9c500300b443d259aef1a5dda92f1b92b123
Cory Hack <cory.hack@gmail.com>
2019-01-04 21:05:56 -0500

    Fix style positios of gritter alter notice

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -68,26 +68,12 @@ module ApplicationHelper
     alert = (flash[:alert] || flash[:error] || flash[:notice])
 
     if alert
-       alert_generator alert
+       alert_generator(alert)
     end
   end
 
   def alert_generator msg
-    js add_gritter(msg, title: "Cory Hack Portfolio", sticky: false)
+    js add_gritter(msg, title: "Cory Hack Portfolio"), extend_gritter(position: :top_left) 
   end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 end
